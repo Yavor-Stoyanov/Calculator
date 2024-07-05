905963b4f166cc9b1cc08369da47806c10c6384d
Yavor Stoyanov <jstojanov2@gmail.com>
2024-07-05 17:51:59 +0300

    Basic functionality (not working yet) to using keyboard.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,22 @@ import * as clear from './clear.js';
 import * as del from './delete.js';
 import { signChange } from './signChange.js';
 import { numbersHandler, operationsHandler } from './display.js';
+// import { isOperableKey } from './util.js';
 
 const numbersBtns = document.querySelectorAll('.numbers');
 numbersBtns.forEach(btn => {
-    btn.addEventListener('click', numbersHandler)
+    btn.addEventListener('click', numbersHandler);
 })
 
 const operationsBtns = document.querySelectorAll('.operations');
 operationsBtns.forEach(btn => {
     btn.addEventListener('click', operationsHandler);
+});
+
+document.addEventListener('keyup', (e) => {
+    console.log(e.keyCode);
+    // if (isOperableKey(e)) {
+        
+    // }
+    
 });
---
+++ src/util.js
@@ -0,0 +1,23 @@
+// export const isOperableKey = (e) => {
+//     const key = e.
+//     const allowedBtns = [
+//         8, 13, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99,
+//         100, 101, 102, 103, 104, 105, 107, 109, 110, 111, 187, 189, 190, 191
+//     ];
+
+//     return allowedBtns.
+// };
+
+
+// 48 - 57 => digits
+// 96 - 105 => numpad digits
+// 110, 190 => dot
+// 13 => enter(equal)
+// shiftKey: true + 107; shiftKey: true + 109 => change of sign
+// 187, 107 => '+'
+// 189, 109 => '-'
+// shiftKey: true + 56 => '*'
+// 111, 191 => '/'
+// shiftKey: true + 53 => '%'
+// 46 => delete (AC)
+// 8 => backspace(C)
